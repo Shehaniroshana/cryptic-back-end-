537023a222c04f80ae4deecd61e93c0853eba534
Shehaniroshana <shehanshehaniroshan@gmail.com>
2025-05-03 21:38:11 +0530

    create user entity and add bcrypt password hashing

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entity/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+     @InjectRepository(User)
+     private userRepo:Repository<User>
+    ){}
+
+}
